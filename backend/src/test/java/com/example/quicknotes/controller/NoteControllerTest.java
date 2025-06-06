aa82ba8b2749eb31ffaafac3d77e095c4fb020dc
Jeff Gladstone <jeff.gladstone2@gmail.com>
2025-06-06 12:26:38 -0400

    Fix import

--- backend/src/test/java/com/example/quicknotes/controller/NoteControllerTest.java
+++ backend/src/test/java/com/example/quicknotes/controller/NoteControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
